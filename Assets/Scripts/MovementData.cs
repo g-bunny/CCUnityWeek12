fe790c5b6cadbf1f1a284787cba5f1c2d590b44a
g-bunny <miri@newschool.edu>
2018-11-16 08:02:20 -0500

    week 12 for Creative Coding Unity: Vuforia, accelerometer and gyroscope input

---
+++ Assets/Scripts/MovementData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementData : MonoBehaviour {
+
+    Vector3 accelValues;
+    Vector3 gyroValues;
+
+    [SerializeField] Text AccelX;
+    [SerializeField] Text AccelY;
+    [SerializeField] Text AccelZ;
+
+    [SerializeField] Text GyroX;
+    [SerializeField] Text GyroY;
+    [SerializeField] Text GyroZ;
+
+    float lowPassFilterFactor = 1.0f / 60.0f;
+    float shakeDetectionThreshold = 1f;
+    Vector3 lowPassValue;
+
+    public bool isShaking;
+
+	void Start () {
+        lowPassValue = Input.acceleration;
+	}
+	
+	void Update () {
+        accelValues = Input.acceleration;
+        gyroValues = Input.gyro.attitude.eulerAngles;
+        isShaking = ShakeCheck();
+	}
+
+    private void LateUpdate()
+    {
+        AccelX.text = "Accel x: " + accelValues.x.ToString("F2");
+        AccelY.text = "Accel y: " + accelValues.y.ToString("F2");
+        AccelZ.text = "Accel z: " + accelValues.z.ToString("F2");
+
+        GyroX.text = "Gyro x: " + gyroValues.x.ToString("F2");
+        GyroY.text = "Gyro y: " + gyroValues.y.ToString("F2");
+        GyroZ.text = "Gyro z: " + gyroValues.z.ToString("F2");
+    }
+
+    bool ShakeCheck(){
+        lowPassValue = Vector3.Lerp(lowPassValue, accelValues, lowPassFilterFactor);
+        Vector3 changeInAccel = accelValues - lowPassValue;
+        if ( changeInAccel.sqrMagnitude >= shakeDetectionThreshold )
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Tiger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiger : MonoBehaviour {
+    Animator anim;
+    [SerializeField] GameObject movementData;
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+	}
+	
+	void Update () {
+        anim.SetBool("Run", movementData.GetComponent<MovementData>().isShaking);
+	}
+}
